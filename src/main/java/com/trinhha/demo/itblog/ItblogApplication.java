288f109eb0c0c4c0417f5363ed2d5f389f685386
trinhha <trinh.the.ha@mgm-tp.com>
2018-07-10 21:45:19 +0700

    Init Project

---
+++ src/main/java/com/trinhha/demo/itblog/ItblogApplication.java
@@ -0,0 +1,12 @@
+package com.trinhha.demo.itblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItblogApplication.class, args);
+    }
+}

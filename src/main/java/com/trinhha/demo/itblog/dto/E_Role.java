dd5c946e45aea4fcc49d9af1b6217fa1b6b6136f
trinhha <trinh.the.ha@mgm-tp.com>
2018-08-20 20:47:59 +0700

    refresh

--- src/main/java/com/trinhha/demo/itblog/dto/E_Role.java
+++
@@ -1,8 +0,0 @@
-package com.trinhha.demo.itblog.dto;
-
-public enum E_Role {
-    ROLE_ADMIN,
-    ROLE_MEMBER,
-    ROLE_MODE,
-    ROLE_GUEST
-}
--- src/main/java/com/trinhha/demo/itblog/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.trinhha.demo.itblog.service;
-
-import com.trinhha.demo.itblog.dto.Role;
-
-import java.util.List;
-
-public interface RoleService {
-    List<Role> getAllRoles();
-}
